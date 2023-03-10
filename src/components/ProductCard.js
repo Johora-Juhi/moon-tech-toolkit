fec6399847f94feebf2936c95716a5b1da47d2cd
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-03-10 23:20:20 +0600

    cart and filter

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,34 +1,60 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { addToCart, removeFromCart } from "../features/cart/cartSlice";
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const { pathname } = useLocation();
   return (
     <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
+      className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900 relative"
       key={product._id}
     >
-      <div className='h-52 w-52 mx-auto'>
+      {pathname.includes("cart") && (
+        <div className="bg-indigo-500 grid place-items-center h-8 w-8 absolute text-white rounded-full top-2 right-2">
+          <p>{product.quantity}</p>
+        </div>
+      )}
+      <div className="h-52 w-52 mx-auto">
         <img src={product.image} alt={product.model} />
       </div>
-      <h1 className='font-bold text-center'>{product.model}</h1>
-      <p className='text-center font-semibold mb-3'>Rating: {product.rating}</p>
-      <div className=' flex-1'>
-        <ul className='space-y-2'>
+      <h1 className="font-bold text-center">{product.model}</h1>
+      <p className="text-center font-semibold mb-3">Rating: {product.rating}</p>
+      <div className=" flex-1">
+        <ul className="space-y-2">
           {product.keyFeature.map((feature) => {
-            return <li className='text-sm '>{feature}</li>;
+            return <li className="text-sm ">{feature}</li>;
           })}
         </ul>
       </div>
-      <div className='flex gap-2 mt-5'>
-        <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
-          Add to cart
-        </button>
-        <button
-          title='Add to wishlist'
-          className='bg-indigo-500  py-1 px-2 rounded-full'
-        >
-          <BiListPlus className='text-white' />
-        </button>
+      <div className="flex gap-2 mt-5">
+        {!pathname.includes("cart") && (
+          <button
+            onClick={() => dispatch(addToCart(product))}
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            Add to cart
+          </button>
+        )}
+        {pathname.includes("cart") && (
+          <button
+            onClick={() => dispatch(removeFromCart(product))}
+            className="bg-red-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            Remove from cart
+          </button>
+        )}
+        {!pathname.includes("cart") && (
+          <button
+            title="Add to wishlist"
+            className="bg-indigo-500  py-1 px-2 rounded-full"
+          >
+            <BiListPlus className="text-white" />
+          </button>
+        )}
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
+import { toggle, toggleBrand } from "../features/filter/filterSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(state=> state.filter)
+  const {stock, brands} = filter;
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -14,26 +19,61 @@ const Home = () => {
 
   const activeClass = "text-white  bg-indigo-500 border-white";
 
+  let content;
+
+  if (products.length) {
+    content = products.map((product) => (
+      <ProductCard key={product._id} product={product}></ProductCard>
+    ));
+  }
+
+  if (products.length && (stock || brands.length)) {
+    content = products
+      .filter((product) => {
+        if (stock) {
+          return product.status === true;
+        }
+        return product;
+      })
+      .filter((product) => {
+        if (brands.length) {
+          return brands.includes(product.brand);
+        }
+        return product;
+      })
+      .map((product) => (
+        <ProductCard key={product._id} product={product}></ProductCard>
+      ));
+  }
   return (
     <div className="max-w-7xl gap-14 mx-auto my-10">
       <div className="mb-10 flex justify-end gap-5">
         <button
-          className={`border px-3 py-2 rounded-full font-semibold
-        `}
+          onClick={() => dispatch(toggle())}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            stock ? activeClass : null
+          } `}
         >
           In Stock
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold `}>
+        <button
+          onClick={() => dispatch(toggleBrand("amd"))}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("amd") ? activeClass : null
+          }`}        >
           AMD
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold `}>
+        <button
+          onClick={() => dispatch(toggleBrand("intel"))}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("intel") ? activeClass : null
+          }`}
+        >
           Intel
         </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-        {products.map((product) => (
-          <ProductCard product={product} />
-        ))}
+        {content}
       </div>
     </div>
   );
